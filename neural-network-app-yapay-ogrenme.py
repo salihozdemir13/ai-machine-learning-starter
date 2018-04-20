5bc31105d463466ee0cf24e16115fec7ae0cc748
Salih Özdemir <ozdemirsalihbsm@gmail.com>
2018-04-20 22:42:58 +0300

    Spell checked.

--- neural-network-app-yapay-ogrenme.py
+++ neural-network-app-yapay-ogrenme.py
@@ -12,23 +12,19 @@ situation   1 0 0     ? - (answer is 1 shhhhh :))
 """
 # Bilgisayara verdiğimiz 4 farklı örnek durumda çıktıları öğrenmesini ve öğrendiği bilgilerle yeni durumunda vereceğimiz inputu doğru cevaplamasını bekliyoruz.
 
-girdi = array([[0,0,1], [1,1,1], [1,0,1]])
+girdi = array([[0,0,1], [1,1,1], [1,0,1]]) # 1 e 3 matris girişi
 
 gercekSonuc = array([[0,1,1]]).T # T Transpose
 
 sinapsAgirlik = array([[1.0,1.0,1.0]]).T
 
-for tekrar in range(1000): # range sayısı artırılınca sinaps ağırlık oranı otimize olacak
+for tekrar in range(1000): # range sayısı artırılınca sinaps ağırlık oranı otimize olacak, hata oranı düşecek
     hucreDegeri = dot(girdi,sinapsAgirlik) # dot product matris çarpım işlemi
-    #print("Hücre değeri")
     #print(hucreDegeri)
     tahmin = 1 / (1 + exp(-hucreDegeri))
-    #print("Tahmin")
     #print(tahmin)
-    #print("Hata oranı")
     #print((gercekSonuc - tahmin) * tahmin * (1 - tahmin))
     sinapsAgirlik += dot(girdi.T, ((gercekSonuc - tahmin) * tahmin * (1 - tahmin)))
-    #print("Sinaps Ağırlığı")
     #print(sinapsAgirlik)
     print(str(np.mean(np.abs(gercekSonuc-tahmin)))) # hata oranının nasıl düştüğünü gözlemleyebiliriz
 
