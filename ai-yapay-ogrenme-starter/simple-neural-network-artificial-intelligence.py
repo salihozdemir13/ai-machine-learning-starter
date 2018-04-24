6569aa14f9fc76dccfad200621b576eee4b6c3b6
Salih Özdemir <ozdemirsalihbsm@gmail.com>
2018-04-24 14:34:52 +0300

    Update file with Python3.

--- ai-yapay-ogrenme-starter/simple-neural-network-artificial-intelligence.py
+++ ai-yapay-ogrenme-starter/simple-neural-network-artificial-intelligence.py
@@ -53,8 +53,8 @@ if __name__ == "__main__":
     #Intialise a single neuron neural network.
     neural_network = NeuralNetwork()
 
-    print "Random starting synaptic weights: "
-    print neural_network.synaptic_weights
+    print("Random starting synaptic weights: ")
+    print(neural_network.synaptic_weights)
 
     # The training set. We have 4 examples, each consisting of 3 input values
     # and 1 output value.
@@ -65,9 +65,10 @@ if __name__ == "__main__":
     # Do it 10,000 times and make small adjustments each time.
     neural_network.train(training_set_inputs, training_set_outputs, 10000)
 
-    print "New synaptic weights after training: "
-    print neural_network.synaptic_weights
+    print("New synaptic weights after training: ")
+    print(neural_network.synaptic_weights)
 
     # Test the neural network with a new situation.
-    print "Considering new situation [1, 0, 0] -> ?: "
-print neural_network.think(array([1, 0, 0]))
+    print("Considering new situation [1, 0, 0] -> ?: ")
+
+print(neural_network.think(array([1, 0, 0])))
