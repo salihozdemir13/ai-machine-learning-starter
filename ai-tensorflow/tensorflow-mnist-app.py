511d67c9153d21e8cff914a8d2c9d70df94399c7
Salih Özdemir <ozdemirsalihbsm@gmail.com>
2018-04-24 21:20:02 +0300

    Update MNIST app.

--- ai-tensorflow/tensorflow-mnist-app.py
+++ ai-tensorflow/tensorflow-mnist-app.py
@@ -5,8 +5,8 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 import tensorflow as tf
-# import numpy as np
-# import scipy.ndimage
+import numpy as np
+import scipy.ndimage
 
 x = tf.placeholder(tf.float32, [None, 784])
 
