14173c390ab4a1108a060106de4c6e25d462f9da
Salih Özdemir <ozdemirsalihbsm@gmail.com>
2018-05-02 14:21:31 +0300

    Created sentiment.pickle with nltk_sentiment_feature_sets.py. We are done with preprocessing.

--- ai-tensorflow/nltk-sentiment-featuresets/nltk_sentiment_feature_sets.py
+++ ai-tensorflow/nltk-sentiment-featuresets/nltk_sentiment_feature_sets.py
@@ -5,6 +5,7 @@ import numpy as np
 import random
 import pickle
 from collections import Counter
+import tensorflow as tf
 
 lemmatizer = WordNetLemmatizer()
 hm_lines = 10000000
@@ -28,6 +29,7 @@ def create_lexicon(pos,neg):
         if 1000 > w_counts[w] > 50:
             l2.append(w)
 
+    print(len(l2))
     return l2
 
 
@@ -49,6 +51,34 @@ def sample_handling(sample, lexicon, classification):
 
     return featureset
 
+def create_feature_sets_and_labels(pos, neg, test_size=0.1):
+    lexicon = create_lexicon(pos, neg)
+    features = []
+    features += sample_handling('pos.txt', lexicon, [1, 0])
+    features += sample_handling('neg.txt', lexicon, [0, 1])
+    random.shuffle(features)
+
+    """
+    does tf.argmax([output]) == tf.argmax([expectations])
+    tf.argmax([54233,21522]) == tf.argmax([1, 0])
+    """
+    features = np.array(features)
+
+    testing_size = int(test_size * len(features))
+
+    train_x = list(features[:, 0][:-testing_size])
+    train_y = list(features[:, 1][:-testing_size])
+
+    test_x = list(features[:, 0][-testing_size:])
+    test_y = list(features[:, 1][-testing_size:])
+
+    return train_x,train_y,test_x,test_y
+
+if __name__ == '__main__':
+
+    train_x, train_y, test_x, test_y = create_feature_sets_and_labels('pos.txt', 'neg.txt')
+    with open('sentiment_set.pickle', 'wb') as f:
+        pickle.dump([train_x,train_y,test_x,test_y], f)
 
 
 
