2e7e88bb3de8d5982ba347b4c589c5c65c45cdbd
Salih Özdemir <ozdemirsalihbsm@gmail.com>
2018-05-04 17:11:18 +0300

    Added RNN with tensorflow and rename neural networks.

--- ai-tensorflow/tf-recurrent-neural-networks.py
+++ ai-tensorflow/tf-recurrent-neural-networks.py
@@ -14,31 +14,12 @@ rnn_size = 128
 x = tf.placeholder('float', [None, 784])
 y = tf.placeholder('float')
 
-def neural_network_model(data):
+def recurrent_neural_network(x):
 
-    hidden_1_layer = {'weights' : tf.Variable(tf.random_normal([784, n_nodes_hl1])),
-                      'biases' : tf.Variable(tf.random_normal([n_nodes_hl1]))}
+    layer = {'weights' : tf.Variable(tf.random_normal([rnn_size, n_classes])),
+                      'biases' : tf.Variable(tf.random_normal([n_classes]))}
 
-    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
-
-    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
-
-    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                      'biases': tf.Variable(tf.random_normal([n_classes]))}
-
-    # (input_data * weights) + biases
-    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
-    l1 = tf.nn.relu(l1)
-
-    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
-    l2 = tf.nn.relu(l2)
-
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']), hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l3)
-
-    output = tf.matmul(l3, output_layer['weights']) + output_layer['biases']
+    output = tf.matmul(***, layer['weights']) + layer['biases']
 
     return output
 
